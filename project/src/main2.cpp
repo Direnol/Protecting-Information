540b0bd28fcb9f207b9475fe27db2b4f347ca620
Roman Mingazeev <direnol@yandex.ru>
2018-09-13 12:36:17 +0700

    Рефакторинг cmake

--- project/src/main2.cpp
+++ project/src/main2.cpp
@@ -1,20 +1,12 @@
+//
+// Created by stanley on 13.09.18.
+//
+
 #include <Encrypt.h>
-#include <OperationOnFile.h>
-#include <Signature.h>
-#include <Operation.h>
 #include <iostream>
 
 int main() {
     Operation op;
-    std::cout << op.powmod(5, 7, 23) << std::endl; // 17
-    std::cout << op.powmod(5, 13, 23) << std::endl; // 21
-    std::cout << op.gcd(100000, 1000230) << std::endl;
-    user_key ans = op.diff_hell({7, 13}, {5, 23});
-    std::cout << ans.first << ' ' << ans.second << std::endl; // 10 10
-    vector<int64_t> evk = op.evklid(28, 19);
-    std::cout << evk[0] << ' ' << evk[1] << ' ' << evk[2] << std::endl; // 1 -2 3
-    std::cout << op.step_bg(9, 2, 23, 6, 4) << std::endl; // 5
-
     Encrypt encrypt;
     int64_t m = 10;
     int64_t P = 23;
