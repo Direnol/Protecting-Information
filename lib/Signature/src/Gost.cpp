466970ae3c8443c1dde0d79069dfa91b84a56717
evg-kazartseff <evg.kazartseff@yandex.ru>
2018-10-30 09:21:06 +0700

    исправил Гост

--- lib/Signature/src/Gost.cpp
+++ lib/Signature/src/Gost.cpp
@@ -5,14 +5,14 @@
 void Signature::Gost::Sign()
 {
     this->ReadText();
-    this->hash = 1 + this->TakingHash() % this->Q;
+    this->hash = 1 + this->TakingHash() % (this->Q - 1);
     int64_t k;
     do {
         do {
-            k = 1 + op.getRand() % Q;
+            k = 1 + op.getRand() % (Q - 1);
             r = op.powmod(a, k, P) % Q;
         } while (r == 0);
-        this->sign = (k * this->hash + C * r) % Q;
+        this->sign = (k * this->hash + X * r) % Q;
     } while (this->sign == 0);
 
     this->WriteSign();
@@ -22,7 +22,7 @@ bool Signature::Gost::TestSign()
 {
     this->InitFromSignFile();
     this->ReadText();
-    this->hash = this->TakingHash() % this->Q;
+    this->hash = 1 + this->TakingHash() % (this->Q - 1);
     if (r == 0 || r >= Q || this->sign == 0 || this->sign >= Q)
         return false;
 
@@ -30,7 +30,8 @@ bool Signature::Gost::TestSign()
     auto inverse_hash = op.evklid(this->hash, this->Q);
     auto u1 = (this->sign * inverse_hash[2]) % Q;
     auto u2 = ((-r) * inverse_hash[2]) % Q;
-    auto v = ((op.powmod(a, u1, P) * (op.powmod(D, u2, P))) % P) % Q;
+    while (u2 < 0) u2 += Q;
+    auto v = ((op.powmod(a, u1, P) * (op.powmod(Y, u2, P))) % P) % Q;
     return v == r;
 }
 
@@ -73,23 +74,23 @@ void Signature::Gost::InitFromSignFile()
 Signature::Gost::Gost(std::string in_f, std::string out_f) : Signature(std::move(in_f), std::move(out_f))
 {
 
-    Q = op.get_simple();
+    Q = op.get_simple(16384, 32767);
 
-    P = 4;
-    b = op.getRand();
+    b = op.getRand(16384, 32767);
+    P = (b * Q) + 1;
     while (!op.is_simple(P)) {
-        b = op.getRand();
+        b = op.getRand(16384, 32767);
         P = (b * Q) + 1;
     }
+//    std::cout << "Q = " << Q << " b = " << b << " P = " << P << std::endl;
     int64_t g;
     do {
-        a = op.getRand() % P;
-        g = op.powmod(a, Q, P);
+        g = 2 + op.getRand() % P;
+        a = op.powmod(g, b, P);
 //        std::cout << "a: " << a << "; g: " << g << std::endl;
-    } while (g != 1);
-
-    C = 1 + op.getRand() % (Q - 1);
-    D = op.powmod(a, C, P);
+    } while (a == 1);
+    X = 1 + op.getRand() % (Q - 1);
+    Y = op.powmod(a, X, P);
 //    std::cout << "Init" << std::endl;
 }
 
